92775c24f82dc4867f902ef8fed71aab2e3eb564
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-13 00:55:57 +0900

    added affirmation of receiving events

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ app.use(bodyParser.json())
 app.post('/events', (req, res) => {
   const event = req.body
 
-  axios.post('http://localhost:4000/events', event)
-  axios.post('http://localhost:4001/events', event)
-  axios.post('http://localhost:4002/events', event)
+  axios.post('http://localhost:4000/events', event) // Posts service
+  axios.post('http://localhost:4001/events', event) // Comments service
+  axios.post('http://localhost:4002/events', event) // Query service
   res.send({ status: 'OK' })
 })
 
