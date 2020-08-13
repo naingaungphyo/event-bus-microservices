9243158b43823825ad7d48c30bb2768322adf8e2
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-14 00:18:21 +0900

    finish moderation service

--- index.js
+++ index.js
@@ -5,13 +5,22 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
   const event = req.body
+
+  events.push(event)
 
   axios.post('http://localhost:4000/events', event) // Posts service
   axios.post('http://localhost:4001/events', event) // Comments service
   axios.post('http://localhost:4002/events', event) // Query service
+  axios.post('http://localhost:4003/events', event) // Moderation service
   res.send({ status: 'OK' })
+})
+
+app.get('/events', (req, res) => {
+  res.send(events)
 })
 
 app.listen(4005, () => {
