b80277d00e82ce39dc993a6ad30ddf35a6c49573
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-17 00:36:08 +0900

    finish k8s before loadbalancer

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ app.post('/events', (req, res) => {
 
   events.push(event)
 
-  axios.post('http://localhost:4000/events', event) // Posts service
-  axios.post('http://localhost:4001/events', event) // Comments service
-  axios.post('http://localhost:4002/events', event) // Query service
-  axios.post('http://localhost:4003/events', event) // Moderation service
+  axios.post('http://posts-clusterip-srv:4000/events', event) // Posts service
+  axios.post('http://comments-srv:4001/events', event) // Comments service
+  axios.post('http://query-srv:4002/events', event) // Query service
+  axios.post('http://moderation-srv:4003/events', event) // Moderation service
   res.send({ status: 'OK' })
 })
 
